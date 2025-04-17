d64aff718a682819a84366b0cb0558c5d0239659
IvanD1061 <ivan.duenas@wsu.edu>
2025-04-17 09:51:29 -0700

    Added to main

--- proxy.c
+++ proxy.c
@@ -1,10 +1,30 @@
-#include <stdio.h>
+#include "csapp.h"
 
-/* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *user_agent_hdr =
+    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) "
+    "Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
-{
-    printf("%s", user_agent_hdr);
-    return 0;
+void handle_request(int connfd);
+void parse_uri(char *uri, char *hostname, char *port, char *path);
+void build_request_headers(rio_t *client_rio, char *host_hdr, char *req_hdrs, char *hostname);
+
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    listenfd = Open_listenfd(argv[1]);
+
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        handle_request(connfd);
+        Close(connfd);
+    }
 }
+
