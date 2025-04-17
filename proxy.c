b46275fc59f3d3168d671ed1c06a3f6e7e4c2f94
IvanD1061 <ivan.duenas@wsu.edu>
2025-04-17 09:52:56 -0700

    request handler

--- proxy.c
+++ proxy.c
@@ -27,4 +27,41 @@ int main(int argc, char **argv) {
         Close(connfd);
     }
 }
+//handles the request.
+void handle_request(int connfd) {
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char hostname[MAXLINE], port[10], path[MAXLINE];
+    char request[MAXLINE], headers[MAXLINE], host_hdr[MAXLINE];
+    int serverfd;
+    rio_t client_rio, server_rio;
 
+    Rio_readinitb(&client_rio, connfd);
+    if (!Rio_readlineb(&client_rio, buf, MAXLINE)) return;
+
+    sscanf(buf, "%s %s %s", method, uri, version);
+    if (strcasecmp(method, "GET")) {
+        fprintf(stderr, "501 Not Implemented: %s\n", method);
+        return;
+    }
+
+    parse_uri(uri, hostname, port, path);
+    build_request_headers(&client_rio, host_hdr, headers, hostname);
+
+    sprintf(request, "GET %s HTTP/1.0\r\n%s%s\r\n", path, host_hdr, headers);
+
+    serverfd = Open_clientfd(hostname, port);
+    if (serverfd < 0) {
+        fprintf(stderr, "Failed to connect to %s:%s\n", hostname, port);
+        return;
+    }
+
+    Rio_readinitb(&server_rio, serverfd);
+    Rio_writen(serverfd, request, strlen(request));
+
+    ssize_t n;
+    while ((n = Rio_readlineb(&server_rio, buf, MAXLINE)) > 0) {
+        Rio_writen(connfd, buf, n);
+    }
+
+    Close(serverfd);
+}
